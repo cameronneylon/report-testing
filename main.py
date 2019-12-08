e782c8fd66941bcebbb61e4a425b72207b849b6c
Ana Nelson <ana@ananelson.com>
2019-12-07 20:18:16 -0800

    Add demos.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-from precipy.main import render
+import demos
+from precipy.main import render_fn
+
+analytics_modules = [demos]
+
+def render(request):
+   return render_fn(request, analytics_mods=analytics_modules)
+
+
+
+## for local testing... python main.py hello.json
+if __name__ == '__main__':
+    from precipy.mock import Request
+    import sys
+    render_fn(Request(sys.argv[1]), analytics_mods=analytics_modules)
